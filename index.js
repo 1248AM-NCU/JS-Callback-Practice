f74f58ba12eb0ecb4d6d8cf509b6d185ca6116e7
TheLegendary1248 <1248alex@gmail.com>
2022-09-10 05:45:04 -0400

    Done with bonuses

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
@@ -13,3 +31,79 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+
+
+
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+        element.style.zIndex = -bottom.toString()
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        element.style.zIndex = -y.toString()
+        
+        function moveCharacter(){ 
+            switch (direction) {
+                case 'west':
+                    x = x - 1;
+                    break;
+                case 'east':
+                    x = x + 1;
+                    break;
+                case 'north':
+                    y = y + 1;
+                    break;
+                case 'south' : 
+                    y = y - 1;
+                    break;
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+            element.style.zIndex = -y.toString()
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            if(callback != null)callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            if(callback != null) callback(direction)
+        })
+    }
+    
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
+}
